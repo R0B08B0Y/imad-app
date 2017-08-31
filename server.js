4ec4d613bf0c2cf69678fefd637d52204d66bea4
R0B08B0Y <30742498+R0B08B0Y@users.noreply.github.com>
2017-08-31 20:58:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db',function(req,res){
+    //make a request
+    //return a response
+    
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
